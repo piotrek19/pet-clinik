20b00346ccf97d7fe896b9852a538bb9259de7b8
Piotr Dzioba <dziobap@gmail.com>
2019-09-24 12:23:50 +0200

    Initialization of project

---
+++ src/main/java/net/dzioba/petclinic/PetClinikApplication.java
@@ -0,0 +1,13 @@
+package net.dzioba.petclinic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PetClinikApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PetClinikApplication.class, args);
+    }
+
+}
---
+++ src/test/java/net/dzioba/petclinic/PetClinikApplicationTests.java
@@ -0,0 +1,16 @@
+package net.dzioba.petclinic;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class PetClinikApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
