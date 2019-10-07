3121ab5e9c8c96b82310027f85cad7ccec2f6493
Piotr Dzioba <dziobap@gmail.com>
2019-10-07 23:30:22 +0200

    interfaces for model services refactored -  common generic interface CrudService<T, ID> added

--- pet-clinic-data/src/main/java/net/dzioba/petclinic/services/OwnerService.java
+++ pet-clinic-data/src/main/java/net/dzioba/petclinic/services/OwnerService.java
@@ -2,15 +2,8 @@ package net.dzioba.petclinic.services;
 
 import net.dzioba.petclinic.model.Owner;
 
-import java.util.Set;
-
-public interface OwnerService {
+public interface OwnerService extends CrudService<Owner, Long> {
 
     Owner findByLastName(String lastName);
 
-    Owner findById(Long id);
-
-    Set<Owner> findAll();
-
-    Owner save(Owner owner);
 }
--- pet-clinic-data/src/main/java/net/dzioba/petclinic/services/PetService.java
+++ pet-clinic-data/src/main/java/net/dzioba/petclinic/services/PetService.java
@@ -2,13 +2,6 @@ package net.dzioba.petclinic.services;
 
 import net.dzioba.petclinic.model.Pet;
 
-import java.util.Set;
+public interface PetService extends CrudService<Pet, Long> {
 
-public interface PetService {
-
-    Pet findById(Long id);
-
-    Set<Pet> findAll();
-
-    Pet save(Pet pet);
 }
