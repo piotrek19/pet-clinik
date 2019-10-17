f4625c37c6258e8b55aa643fadfe93c81077267d
Piotr Dzioba <dziobap@gmail.com>
2019-10-17 12:14:16 +0200

    few controllers and related pages (templates) added

---
+++ pet-clinic-web/src/main/java/net/dzioba/petclinic/controllers/VetController.java
@@ -0,0 +1,15 @@
+package net.dzioba.petclinic.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/vets")
+public class VetController {
+
+    @RequestMapping({"", "/", "/index", "/index.html"})
+    public String listVets(){
+
+        return "vets/index";
+    }
+}
