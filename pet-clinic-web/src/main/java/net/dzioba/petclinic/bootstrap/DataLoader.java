c731feb0a8cdcc3479cdada5bec3aeb4fa8c68b3
Piotr Dzioba <dziobap@gmail.com>
2019-10-25 18:36:50 +0200

    loading PetType concrete data at application startup added

--- pet-clinic-web/src/main/java/net/dzioba/petclinic/bootstrap/DataLoader.java
+++ pet-clinic-web/src/main/java/net/dzioba/petclinic/bootstrap/DataLoader.java
@@ -1,8 +1,10 @@
 package net.dzioba.petclinic.bootstrap;
 
 import net.dzioba.petclinic.model.Owner;
+import net.dzioba.petclinic.model.PetType;
 import net.dzioba.petclinic.model.Vet;
 import net.dzioba.petclinic.services.OwnerService;
+import net.dzioba.petclinic.services.PetTypeService;
 import net.dzioba.petclinic.services.VetService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
@@ -13,15 +15,27 @@ public class DataLoader implements CommandLineRunner {
 
     private final OwnerService ownerService;
     private final VetService vetService;
+    private final PetTypeService petTypeService;
 
     @Autowired
-    public DataLoader(OwnerService ownerService, VetService vetService) {
+    public DataLoader(OwnerService ownerService, VetService vetService, PetTypeService petTypeService) {
         this.ownerService = ownerService;
         this.vetService = vetService;
+        this.petTypeService = petTypeService;
     }
 
     @Override
     public void run(String... args) throws Exception {
+        PetType dogPetType = new PetType();
+        dogPetType.setName("dog");
+        petTypeService.save(dogPetType);
+
+        PetType catPetType = new PetType();
+        catPetType.setName("cat");
+        petTypeService.save(catPetType);
+
+        System.out.println("Loaded: PetTypes...");
+
         Owner owner1 = new Owner();
         owner1.setFirstName("Michael");
         owner1.setLastName("Weston");
