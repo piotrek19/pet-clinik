78fc9c7aaf5246acfaa5bfc3474d3182e6f45055
Piotr Dzioba <dziobap@gmail.com>
2019-10-25 17:55:56 +0200

    added Speciality entity and modified Vet entity to has a set of it

--- pet-clinic-data/src/main/java/net/dzioba/petclinic/model/Vet.java
+++ pet-clinic-data/src/main/java/net/dzioba/petclinic/model/Vet.java
@@ -1,6 +1,19 @@
 package net.dzioba.petclinic.model;
 
+import java.util.Set;
+
 public class Vet extends Person {
 
+    private Set<Speciality> specialities;
 
+    public Vet() {
+    }
+
+    public Set<Speciality> getSpecialities() {
+        return specialities;
+    }
+
+    public void setSpecialities(Set<Speciality> specialities) {
+        this.specialities = specialities;
+    }
 }
