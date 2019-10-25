c69dfd1d42aace14901988ddd1dc4b9d757c0a26
Piotr Dzioba <dziobap@gmail.com>
2019-10-25 18:06:05 +0200

    added properties to Owner entity

--- pet-clinic-data/src/main/java/net/dzioba/petclinic/model/Owner.java
+++ pet-clinic-data/src/main/java/net/dzioba/petclinic/model/Owner.java
@@ -4,9 +4,27 @@ import java.util.Set;
 
 public class Owner extends Person {
 
+    private String address;
+    private String telephone;
     private Set<Pet> pets;
 
     public Owner() {
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getTelephone() {
+        return telephone;
+    }
+
+    public void setTelephone(String telephone) {
+        this.telephone = telephone;
     }
 
     public Set<Pet> getPets() {
