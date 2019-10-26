0399dd208c3838a626bb897cd992e89c034b4ceb
Piotr Dzioba <dziobap@gmail.com>
2019-10-27 00:44:54 +0200

    Visit jpa annotations added

--- pet-clinic-data/src/main/java/net/dzioba/petclinic/model/Pet.java
+++ pet-clinic-data/src/main/java/net/dzioba/petclinic/model/Pet.java
@@ -2,6 +2,8 @@ package net.dzioba.petclinic.model;
 
 import javax.persistence.*;
 import java.time.LocalDate;
+import java.util.HashSet;
+import java.util.Set;
 
 @Entity
 @Table(name = "pet")
@@ -20,6 +22,9 @@ public class Pet extends BaseEntity {
     @ManyToOne
     @JoinColumn(name = "owner_id")
     private Owner owner;
+
+    @OneToMany(cascade = CascadeType.ALL, mappedBy = "pet")
+    private Set<Visit> visits = new HashSet<>();
 
     public Pet() {
     }
@@ -61,5 +66,13 @@ public class Pet extends BaseEntity {
 
     public void setOwner(Owner owner) {
         this.owner = owner;
+    }
+
+    public Set<Visit> getVisits() {
+        return visits;
+    }
+
+    public void setVisits(Set<Visit> visits) {
+        this.visits = visits;
     }
 }
