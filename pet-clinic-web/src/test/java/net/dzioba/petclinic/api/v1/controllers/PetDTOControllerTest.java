3a3fa75b4a9166346b8f6d89a67c9850345025c5
Piotr Dzioba <dziobap@gmail.com>
2019-11-30 15:11:04 +0100

    PetDTOControllerTest class refactored

--- pet-clinic-web/src/test/java/net/dzioba/petclinic/api/v1/controllers/PetDTOControllerTest.java
+++ pet-clinic-web/src/test/java/net/dzioba/petclinic/api/v1/controllers/PetDTOControllerTest.java
@@ -15,6 +15,9 @@ import org.springframework.http.MediaType;
 import org.springframework.restdocs.RestDocumentationExtension;
 import org.springframework.restdocs.constraints.ConstraintDescriptions;
 import org.springframework.restdocs.payload.FieldDescriptor;
+import org.springframework.restdocs.payload.ResponseFieldsSnippet;
+import org.springframework.restdocs.request.ParameterDescriptor;
+import org.springframework.restdocs.request.PathParametersSnippet;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.util.StringUtils;
 
@@ -38,72 +41,115 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 @WebMvcTest(PetDTOController.class)
 class PetDTOControllerTest {
 
-    private static final Long PET1_ID = 2L;
-    private static final String PET1_NAME = "PET1_NAME";
-    private static final Long PET2_ID = 12L;
-    private static final String PET2_NAME = "PET2_NAME";
-    private static final Long OWNER_ID = 1L;
-
     @MockBean
     private PetDTOService petDTOService;
 
     @Autowired
     private MockMvc mockMvc;
 
+    // REST DOCS common documentation artifacts:
+    private static ConstrainedFields fields = new ConstrainedFields(PetDTO.class);
 
+    private static class ConstrainedFields {
+
+        private final ConstraintDescriptions constraintDescriptions;
+
+        ConstrainedFields(Class<?> input) {
+            this.constraintDescriptions = new ConstraintDescriptions(input);
+        }
+
+        private FieldDescriptor withPath(String path) {
+            return fieldWithPath(path).attributes(key("constraints").value(StringUtils
+                    .collectionToDelimitedString(this.constraintDescriptions
+                            .descriptionsForProperty(path), ". ")));
+        }
+    }
+
+    private static final ParameterDescriptor ownerParameterDescriptor =
+            parameterWithName("ownerId").description("Id of Owner which has Pets");
+
+    private static final ParameterDescriptor petParameterDescriptor =
+            parameterWithName("petId").description("Id of Pet");
+
+    private static final PathParametersSnippet ownerPathParametersSnippet = pathParameters(ownerParameterDescriptor);
+
+    private static final PathParametersSnippet petPathParametersSnippet = pathParameters(
+            ownerParameterDescriptor, petParameterDescriptor);
+
+    private static final FieldDescriptor[] petDTOResponseFieldsDescriptors = new FieldDescriptor[] {
+            fields.withPath("id").description("Pet id"),
+            fields.withPath("name").description("Pet name"),
+            fields.withPath("birthDate").description("Pet birth date"),
+            fields.withPath("petType").description("PetType"),
+            fields.withPath("petType.id").description("Id of PetType"),
+            fields.withPath("petType.name").description("PetType name"),
+            fields.withPath("owner").description("Pet Owner"),
+            fields.withPath("owner.id").description("Owner id"),
+            fields.withPath("owner.firstName").description("Owner fist name"),
+            fields.withPath("owner.lastName").description("Owner last name"),
+            fields.withPath("owner.detailsUrl").description("URL to GET details of the Owner"),
+            fields.withPath("visits[]").description("Array of Pet Visits")
+    };
+
+    private static ResponseFieldsSnippet petDTOResponseFieldsSnippet = responseFields(petDTOResponseFieldsDescriptors);
+
+    // HELPER methods:
+
+    private static final Long PET1_ID = 2L;
+    private static final String PET1_NAME = "PetName";
+    private static final Long OWNER_ID = 1L;
+
+    private static PetDTO getNewPetDTO() {
+        PetDTO petDTO = new PetDTO();
+        petDTO.setName("PetName");
+        //petDTO.setBirthDate(LocalDate.of(2015, 1,15));    //todo: LocalDate conversion to json fails
+        PetTypeDTO petTypeDTO = new PetTypeDTO(1L, null);
+        petDTO.setPetType(petTypeDTO);
+        return petDTO;
+    }
+
+    private static PetDTO getPersistedPetDTO() {
+        PetDTO resultPetDTO = new PetDTO();
+        resultPetDTO.setId(PET1_ID);
+        resultPetDTO.setName(PET1_NAME);
+        //resultPetDTO.setBirthDate(LocalDate.of(2015, 1,15));    //todo: LocalDate conversion to json fails
+        PetTypeDTO resultPetTypeDTO = new PetTypeDTO(1L, "dog");
+        OwnerShortDTO resultOwnerShortDTO = new OwnerShortDTO(
+                OWNER_ID, "OwnerFirstName", "OwnerLastName", "localhost:8080/api/v1/owners/" + OWNER_ID);
+        resultPetDTO.setOwner(resultOwnerShortDTO);
+        resultPetDTO.setPetType(resultPetTypeDTO);
+        return resultPetDTO;
+    }
+
+    // TESTS:
     @Test
     void findPetsOfGivenOwner() throws Exception {
         //given
         when(petDTOService.findPetsOfGivenOwner(OWNER_ID))
-                .thenReturn(createReferencePets());
+                .thenReturn(List.of(getPersistedPetDTO()));
 
         //when then
         mockMvc.perform(get(PetDTOController.BASE_URL, OWNER_ID)
                 .contentType(MediaType.APPLICATION_JSON)
                 .accept(MediaType.APPLICATION_JSON))
                 .andExpect(status().isOk())
-                .andExpect(jsonPath("$.pets", hasSize(2)))
+                .andExpect(jsonPath("$.pets", hasSize(1)))
                 .andDo(document("v1/owners/ownerId/pets-getMany",
-                        pathParameters(
-                                parameterWithName("ownerId").description("Id of Owner which Pets are wanted to get.")
-                        ),
-                        responseFields(
-                                fieldWithPath("pets[]").description("Array containing all Pets of given Owner."),
-                                fieldWithPath("pets[].id").description("Pet id"),
-                                fieldWithPath("pets[].name").description("Pet name"),
-                                fieldWithPath("pets[].birthDate").description("Pet birth date"),
-                                fieldWithPath("pets[].petType").description("Pet type"),
-                                fieldWithPath("pets[].owner").description("Pet Owner"),
-                                fieldWithPath("pets[].visits[]").description("Array of Pet Visits")
-                        )
-
-                ));
+                        ownerPathParametersSnippet,
+                        responseFields(fieldWithPath("pets[]").description("Array containing all Pets of given Owner"))
+                                .andWithPrefix("pets[].", petDTOResponseFieldsDescriptors))
+                );
 
         verify(petDTOService, times(1)).findPetsOfGivenOwner(any());
 
     }
 
-    private List<PetDTO> createReferencePets() {
-        PetDTO petDTO1 = new PetDTO();
-        petDTO1.setId(PET1_ID);
-        petDTO1.setName(PET1_NAME);
-
-        PetDTO petDTO2 = new PetDTO();
-        petDTO2.setId(PET2_ID);
-        petDTO2.setName(PET2_NAME);
-
-        return List.of(petDTO1, petDTO2);
-    }
-
     @Test
     void findById() throws Exception {
-        PetDTO petDTO = new PetDTO();
-        petDTO.setId(PET1_ID);
-        petDTO.setName(PET1_NAME);
 
         //given
         when(petDTOService.findById(PET1_ID))
-                .thenReturn(petDTO);
+                .thenReturn(getPersistedPetDTO());
 
         //when then
         mockMvc.perform(get(PetDTOController.BASE_URL + "/{petId}", OWNER_ID, PET1_ID)
@@ -113,19 +159,8 @@ class PetDTOControllerTest {
                 .andExpect(jsonPath("$.id", equalTo((Integer.valueOf(PET1_ID.toString())))))
                 .andExpect(jsonPath("$.name", equalTo(PET1_NAME)))
                 .andDo(document("v1/owners/ownerId/pets-get",
-                        pathParameters(
-                                parameterWithName("ownerId").description("Id of Owner which Pet is wanted to get."),
-                                parameterWithName("petId").description("Id of Pet is wanted to get.")
-
-                        ),
-                        responseFields(
-                                fieldWithPath("id").description("Pet id"),
-                                fieldWithPath("name").description("Pet name"),
-                                fieldWithPath("birthDate").description("Pet birth date"),
-                                fieldWithPath("petType").description("Pet type"),
-                                fieldWithPath("owner").description("Pet Owner"),
-                                fieldWithPath("visits[]").description("Array of Pet Visits")
-                        )
+                        petPathParametersSnippet,
+                        petDTOResponseFieldsSnippet
                 ));
 
 
@@ -150,24 +185,9 @@ class PetDTOControllerTest {
     @Test
     void createPet() throws Exception {
 
-        PetDTO petDTO = new PetDTO();
-        petDTO.setName("PetName");
-        //petDTO.setBirthDate(LocalDate.of(2015, 1,15));    //todo: LocalDate conversion to json fails
-        PetTypeDTO petTypeDTO = new PetTypeDTO(1L, null);
-        petDTO.setPetType(petTypeDTO);
+        PetDTO petDTO = getNewPetDTO();
 
-        PetDTO resultPetDTO = new PetDTO();
-        resultPetDTO.setId(PET1_ID);
-        resultPetDTO.setName("PetName");
-        //resultPetDTO.setBirthDate(LocalDate.of(2015, 1,15));    //todo: LocalDate conversion to json fails
-        PetTypeDTO resultPetTypeDTO = new PetTypeDTO(1L, "dog");
-        OwnerShortDTO resultOwnerShortDTO = new OwnerShortDTO(
-                OWNER_ID, "OwnerFirstName", "OwnerLasttName", "localhost:8080/api/v1/owners/" + OWNER_ID);
-        resultPetDTO.setOwner(resultOwnerShortDTO);
-        resultPetDTO.setPetType(resultPetTypeDTO);
-
-
-        ConstrainedFields fields = new ConstrainedFields(PetDTO.class);
+        PetDTO resultPetDTO = getPersistedPetDTO();
 
         //given
         when(petDTOService.save(petDTO, OWNER_ID)).thenReturn(resultPetDTO);
@@ -190,20 +210,7 @@ class PetDTOControllerTest {
                                 fields.withPath("owner").ignored(),
                                 fields.withPath("visits[]").ignored()
                         ),
-                        responseFields(
-                                fields.withPath("id").description("Pet id"),
-                                fields.withPath("name").description("Pet name"),
-                                fields.withPath("birthDate").description("Pet birth date"),
-                                fields.withPath("petType").description("PetType"),
-                                fields.withPath("petType.id").description("Id of PetType"),
-                                fields.withPath("petType.name").description("PetType name"),
-                                fields.withPath("owner").description("Pet Owner"),
-                                fields.withPath("owner.id").description("Owner id"),
-                                fields.withPath("owner.firstName").description("Owner fist name"),
-                                fields.withPath("owner.lastName").description("Owner last name"),
-                                fields.withPath("owner.detailsUrl").description("URL to GET details of the Owner"),
-                                fields.withPath("visits[]").description("Array of Pet Visits")
-                        )
+                        petDTOResponseFieldsSnippet
                 ));
 
         verify(petDTOService, times(1)).save(any(), any());
@@ -218,28 +225,10 @@ class PetDTOControllerTest {
                 .accept(MediaType.APPLICATION_JSON))
                 .andExpect(status().isOk())
                 .andDo(document("v1/owners/ownerId/pets-delete",
-                        pathParameters(
-                                parameterWithName("ownerId").description("Id of Owner which Pet is wanted to delete."),
-                                parameterWithName("petId").description("Id of Pet which is wanted to delete.")
-                        )
+                        petPathParametersSnippet
                 ));
 
         verify(petDTOService, times(1)).deleteById(PET1_ID);
-    }
-
-    private static class ConstrainedFields {
-
-        private final ConstraintDescriptions constraintDescriptions;
-
-        ConstrainedFields(Class<?> input) {
-            this.constraintDescriptions = new ConstraintDescriptions(input);
-        }
-
-        private FieldDescriptor withPath(String path) {
-            return fieldWithPath(path).attributes(key("constraints").value(StringUtils
-                    .collectionToDelimitedString(this.constraintDescriptions
-                            .descriptionsForProperty(path), ". ")));
-        }
     }
 
 }
