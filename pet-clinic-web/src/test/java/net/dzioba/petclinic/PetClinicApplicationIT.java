1ffb0d5f49200c149cb839bbba10478b3249da22
Piotr Dzioba <dziobap@gmail.com>
2019-12-16 14:58:28 +0100

    failsafe maven plugin configuration updated

--- pet-clinic-web/src/test/java/net/dzioba/petclinic/PetClinicApplicationIT.java
+++ pet-clinic-web/src/test/java/net/dzioba/petclinic/PetClinicApplicationIT.java
@@ -7,7 +7,7 @@ import org.springframework.test.context.junit.jupiter.SpringExtension;
 
 @ExtendWith(SpringExtension.class)
 @SpringBootTest
-public class PetClinicApplicationTests {
+public class PetClinicApplicationIT {
 
     @Test
     public void contextLoads() {
